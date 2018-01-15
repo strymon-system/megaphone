4a2b393c1319e7112ef90721ee89dc5177552122
Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>
2018-01-15 15:34:28 +0100

    Rewrite example
    Signed-off-by: Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>

--- examples/scaling_state_machine.rs
+++ examples/scaling_state_machine.rs
@@ -2,12 +2,14 @@ extern crate timely;
 extern crate dynamic_scaling_mechanism;
 
 
-use timely::dataflow::operators::{ToStream, Map, Inspect};
+use timely::dataflow::*;
+use timely::dataflow::operators::{Input, Probe, Map, Inspect};
 use timely::dataflow::operators::aggregation::StateMachine;
+
 use dynamic_scaling_mechanism::distribution::{ControlInst, ControlStateMachine};
 
 fn main() {
-    timely::example(|scope| {
+    timely::execute_from_args(std::env::args(), |worker| {
 
         // these results happen to be right, but aren't guaranteed.
         // the system is at liberty to re-order within a timestamp.
@@ -15,30 +17,61 @@ fn main() {
                           (1, 1), (1, 4), (1, 9), (1, 16), (1, 25)];
         let result2 = result.clone();
 
-        let control = vec![ControlInst::new(0, vec![0; 256])].to_stream(scope);
+        let index = worker.index();
+        let mut input = InputHandle::new();
+        let mut control_input = InputHandle::new();
+        let mut probe = ProbeHandle::new();
 
-        (0..10).to_stream(scope)
-            .map(|x| (x % 2, x))
-            .state_machine(
-                |_key, val, agg| {
-                    *agg += val;
-                    (false, Some((*_key, *agg)))
-                },
-                |key| *key as u64
-            )
-            .inspect(move |x| {assert!(result.contains(x)); println!("state_machine x: {:?}", x)});
+        worker.dataflow(|scope| {
+            let control = scope.input_from(&mut control_input);
+            let input = scope.input_from(&mut input);
+            input
+                .map(|x| (x % 2, x))
+                .state_machine(
+                    |_key, val, agg| {
+                        *agg += val;
+                        (false, Some((*_key, *agg)))
+                    },
+                    |key| *key as u64
+                )
+                .inspect(move |x| {
+                    assert!(result.contains(x));
+                    println!("state_machine x: {:?}", x)
+                })
+                .probe_with(&mut probe);
 
-        (0..10).to_stream(scope)
-            .map(|x| (x % 2, x))
-            .control_state_machine(
-                |_key, val, agg| {
-                    *agg += val;
-                    (false, Some((*_key, *agg)))
-                },
-                |key| *key as u64
-                ,
-                &control
-            )
-            .inspect(move |x| {assert!(result2.contains(x)); println!("control_state_machine x: {:?}", x)});
-    });
+            input
+                .map(|x| (x % 2, x))
+                .control_state_machine(
+                    |_key, val, agg| {
+                        *agg += val;
+                        (false, Some((*_key, *agg)))
+                    },
+                    |key| *key as u64
+                    ,
+                    &control
+                )
+                .inspect(move |x| {
+                    assert!(result2.contains(x));
+                    println!("control_state_machine x: {:?}", x)
+                })
+                .probe_with(&mut probe);
+        });
+
+        control_input.send(ControlInst::new(0, vec![0; 256]));
+        control_input.advance_to(5);
+        control_input.send(ControlInst::new(1, vec![0; 256]));
+        control_input.advance_to(10);
+        // introduce data and watch!
+        for round in 0..10 {
+            if index == 0 {
+                input.send(round);
+            }
+            input.advance_to(round + 1);
+            while probe.less_than(input.time()) {
+                worker.step();
+            }
+        }
+
+    }).unwrap();
 }
