493ad2f8f1791ca928a8d41f7a47f8170e5457e9
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2018-02-19 14:06:14 +0100

    word_count square wave experiment

--- examples/word_count.rs
+++ examples/word_count.rs
@@ -54,6 +54,11 @@ impl SentenceGenerator {
     // }
 }
 
+enum ExperimentMode {
+    OpenLoopConstant,
+    OpenLoopSquare,
+}
+
 fn main() {
     let mut args = std::env::args();
     let _cmd = args.next();
@@ -65,7 +70,11 @@ fn main() {
     // Number of distinct keys.
     let keys: usize = args.next().unwrap().parse().unwrap();
     // Open-loop?
-    let open_loop = args.next().unwrap() == "open-loop";
+    let mode = match args.next().unwrap().as_str() {
+        "open-loop" => ExperimentMode::OpenLoopConstant,
+        "open-loop-square" => ExperimentMode::OpenLoopSquare,
+        _ => panic!("invalid mode"),
+    };
 
     timely::execute_from_args(args, move |worker| {
 
@@ -120,129 +129,143 @@ fn main() {
         }
         eprintln!("debug: data loaded");
 
-        if !open_loop {
-            // TODO may be stale
+        // rounds: number of seconds until reconfiguration.
+        // batch: target number of records per second.
+        eprintln!("debug: open-loop");
 
-            if index == 0 {
-                // Redistribute bins to all workers at once.
-                control_input.advance_to(rounds);
-                for i in 0 .. map.len() {
-                    map[i] = i % worker.peers();
-                }
-                control_input.send(Control::new(control_counter,  1, ControlInst::Map(map.clone())));
+        let requests_per_sec = batch;
+        let ns_per_request = 1_000_000_000 / requests_per_sec;
+        let mut request_counter = peers + index;    // skip first request for each.
+
+        // we will run for 3 * rounds seconds, with two reconfigurations.
+        let mut measurements = Vec::with_capacity(3 * rounds * requests_per_sec / peers);
+        let mut to_print = Vec::with_capacity(3 * rounds * requests_per_sec / peers);
+
+        let timer = ::std::time::Instant::now();
+
+        let mut control_plan = Vec::new();
+
+        if index == 0 {
+            for i in 0 .. map.len() {
+                map[i] = i % worker.peers();
+            }
+            control_plan.push((rounds * 1_000_000_000, Control::new(control_counter,  1, ControlInst::Map(map.clone()))));
+            control_counter += 1;
+
+            for i in 0 .. map.len() {
+                map[i] = 0;//i % peers;
+                control_plan.push((2 * rounds * 1_000_000_000, Control::new(control_counter,  1, ControlInst::Map(map.clone()))));
                 control_counter += 1;
-
-                // Attempt progressive migration.
-                for i in 0 .. map.len() {
-                    map[i] = 0;
-                    control_input.advance_to(2 * rounds + i);
-                    control_input.send(Control::new(control_counter,  1, ControlInst::Map(map.clone())));
-                    control_counter += 1;
-                }
-            }
-
-            control_input.close();
-
-            let mut durations = Vec::with_capacity(3 * rounds);
-            for _round in 0 .. 3 * rounds {
-                let start = Instant::now();
-                for _ in 0 .. batch {
-                    input.send(text_gen.word());
-                }
-                let time = input.time().inner;
-                input.advance_to(time + 1);
-                while probe.less_than(input.time()) {
-                    worker.step();
-                }
-
-                durations.push(start.elapsed());
-            }
-
-            for (iter, duration) in durations.drain(..).enumerate() {
-                if iter < 5 || (iter > rounds - 5 && iter < rounds + 5) || (iter > 2*rounds - 5 && iter < 2*rounds + 5) {
-                    println!("iter: {:?}, duration: {:?}", 2 + iter, duration);
-                }
             }
         }
-        else {
 
-            // rounds: number of seconds until reconfiguration.
-            // batch: target number of records per second.
-            eprintln!("debug: open-loop");
+        let mut just_redistributed = false;
+        let mut redistributions = Vec::with_capacity(256);
 
-            let requests_per_sec = batch;
-            let ns_per_request = 1_000_000_000 / requests_per_sec;
-            let mut request_counter = peers + index;    // skip first request for each.
+        // -- square --
+        let SQUARE_PERIOD = 2_000_000_000; // 2 sec
 
-            // we will run for 3 * rounds seconds, with two reconfigurations.
-            let mut measurements = Vec::with_capacity(3 * rounds * requests_per_sec / peers);
-            let mut to_print = Vec::with_capacity(3 * rounds * requests_per_sec / peers);
+        let SQUARE_HEIGHT = 10000;
+        let half_period = SQUARE_PERIOD / 2;
+        let hi_ns_per_request = 1_000_000_000 / (requests_per_sec + SQUARE_HEIGHT);
+        let lo_ns_per_request = 1_000_000_000 / (requests_per_sec - SQUARE_HEIGHT);
 
-            let timer = ::std::time::Instant::now();
+        let ns_times_in_period = {
+            let mut ns_times_in_period = Vec::with_capacity(SQUARE_PERIOD / ns_per_request);
+            let mut cur_ns = 0;
+            while cur_ns < SQUARE_PERIOD && ns_times_in_period.len() < ns_times_in_period.capacity() {
+                ns_times_in_period.push(cur_ns);
+                cur_ns += if cur_ns < half_period { hi_ns_per_request } else { lo_ns_per_request };
+            }
+            assert_eq!(ns_times_in_period.len(), SQUARE_PERIOD / ns_per_request);
+            ns_times_in_period
+        };
+        eprintln!("{:?}", ns_times_in_period);
+        std::process::exit(-1);
+        // ------------
 
-            let mut control_plan = Vec::new();
+        while measurements.len() < measurements.capacity() {
 
-            if index == 0 {
-                for i in 0 .. map.len() {
-                    map[i] = i % worker.peers();
+            // Open-loop latency-throughput test, parameterized by offered rate `ns_per_request`.
+            let elapsed = timer.elapsed();
+            let elapsed_ns = elapsed.as_secs() * 1_000_000_000 + (elapsed.subsec_nanos() as u64);
+
+            // If the next planned migration can now be effected, ...
+            if control_plan.get(0).map(|&(time, _)| time < elapsed_ns as usize).unwrap_or(false) {
+                if just_redistributed {
+                    just_redistributed = false;
                 }
-                control_plan.push((rounds * 1_000_000_000, Control::new(control_counter,  1, ControlInst::Map(map.clone()))));
-                control_counter += 1;
-
-                for i in 0 .. map.len() {
-                    map[i] = 0;//i % peers;
-                    control_plan.push((2 * rounds * 1_000_000_000, Control::new(control_counter,  1, ControlInst::Map(map.clone()))));
-                    control_counter += 1;
+                else {
+                    redistributions.push(elapsed_ns);
+                    control_input.send(control_plan.remove(0).1);
+                    just_redistributed = true;
                 }
             }
 
-            let mut just_redistributed = false;
-            let mut redistributions = Vec::with_capacity(256);
+            match mode {
+                ExperimentMode::OpenLoopConstant => {
 
-            while measurements.len() < measurements.capacity() {
-
-                // Open-loop latency-throughput test, parameterized by offered rate `ns_per_request`.
-                let elapsed = timer.elapsed();
-                let elapsed_ns = elapsed.as_secs() * 1_000_000_000 + (elapsed.subsec_nanos() as u64);
-
-                // If the next planned migration can now be effected, ...
-                if control_plan.get(0).map(|&(time, _)| time < elapsed_ns as usize).unwrap_or(false) {
-                    if just_redistributed {
-                        just_redistributed = false;
+                    // Introduce any requests that have "arrived" since last we were here.
+                    // Request i "arrives" at `index + ns_per_request * (i + 1)`.
+                    while ((request_counter * ns_per_request) as u64) < elapsed_ns {
+                        // input.send(text_gen.generate());
+                        input.send(text_gen.word());
+                        request_counter += peers;
                     }
-                    else {
-                        redistributions.push(elapsed_ns);
-                        control_input.send(control_plan.remove(0).1);
-                        just_redistributed = true;
+                    input.advance_to(elapsed_ns as usize);
+                    control_input.advance_to(elapsed_ns as usize);
+
+                    while probe.less_than(input.time()) {
+                        worker.step();
                     }
-                }
 
-                // Introduce any requests that have "arrived" since last we were here.
-                // Request i "arrives" at `index + ns_per_request * (i + 1)`.
-                while ((request_counter * ns_per_request) as u64) < elapsed_ns {
-                    // input.send(text_gen.generate());
-                    input.send(text_gen.word());
-                    request_counter += peers;
-                }
-                input.advance_to(elapsed_ns as usize);
-                control_input.advance_to(elapsed_ns as usize);
+                    // Determine completed ns.
+                    let acknowledged_ns: u64 = probe.with_frontier(|frontier| frontier[0].inner as u64);
 
-                while probe.less_than(input.time()) {
-                    worker.step();
-                }
+                    let elapsed = timer.elapsed();
+                    let elapsed_ns = elapsed.as_secs() * 1_000_000_000 + (elapsed.subsec_nanos() as u64);
 
-                // Determine completed ns.
-                let acknowledged_ns: u64 = probe.with_frontier(|frontier| frontier[0].inner as u64);
+                    // any un-recorded measurements that are complete should be recorded.
+                    while (((index + peers * (measurements.len() + 1)) * ns_per_request) as u64) < acknowledged_ns && measurements.len() < measurements.capacity() {
+                        let requested_at = ((index + peers * (measurements.len() + 1)) * ns_per_request) as u64;
+                        measurements.push(elapsed_ns - requested_at);
+                        to_print.push((requested_at, elapsed_ns - requested_at))
+                    }
+                },
+                ExperimentMode::OpenLoopSquare => {
+                    // ---|   |---|    <- hi_requests_per_sec
+                    //    |   |   |
+                    //    |---|   |--- <- lo_requesrs_per_sec
+                    // <------>
+                    //  period
 
-                let elapsed = timer.elapsed();
-                let elapsed_ns = elapsed.as_secs() * 1_000_000_000 + (elapsed.subsec_nanos() as u64);
+                    let ns_times_in_period_index = |counter| counter % ns_times_in_period.len();
+                    let time_base = |counter| counter / ns_times_in_period.len() * SQUARE_PERIOD;
 
-                // any un-recorded measurements that are complete should be recorded.
-                while (((index + peers * (measurements.len() + 1)) * ns_per_request) as u64) < acknowledged_ns && measurements.len() < measurements.capacity() {
-                    let requested_at = ((index + peers * (measurements.len() + 1)) * ns_per_request) as u64;
-                    measurements.push(elapsed_ns - requested_at);
-                    to_print.push((requested_at, elapsed_ns - requested_at))
-                }
+                    while time_base(request_counter + peers) + ns_times_in_period[ns_times_in_period_index(request_counter + peers)] < (elapsed_ns as usize) {
+                        input.send(text_gen.word());
+                        request_counter += peers;
+                    }
+                    input.advance_to(elapsed_ns as usize);
+                    control_input.advance_to(elapsed_ns as usize);
+
+                    while probe.less_than(input.time()) {
+                        worker.step();
+                    }
+
+                    // Determine completed ns.
+                    let acknowledged_ns: u64 = probe.with_frontier(|frontier| frontier[0].inner as u64);
+
+                    let elapsed = timer.elapsed();
+                    let elapsed_ns = elapsed.as_secs() * 1_000_000_000 + (elapsed.subsec_nanos() as u64);
+
+                    // any un-recorded measurements that are complete should be recorded.
+                    while (time_base(index + peers * (measurements.len() + 1)) + ns_times_in_period[ns_times_in_period_index(index + peers * (measurements.len() + 1))]) < acknowledged_ns as usize && measurements.len() < measurements.capacity() {
+                        let requested_at = (time_base(index + peers * (measurements.len() + 1)) + ns_times_in_period[ns_times_in_period_index(index + peers * (measurements.len() + 1))]) as u64;
+                        measurements.push(elapsed_ns - requested_at);
+                        to_print.push((requested_at, elapsed_ns - requested_at))
+                    }
+                },
             }
 
             measurements.sort();
