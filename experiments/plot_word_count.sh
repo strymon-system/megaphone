6953208fe715d5b7a15eef26829a93e0f46b3339
Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>
2018-02-27 14:57:30 +0100

    Fix font size in plots
    Signed-off-by: Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>

--- experiments/plot_word_count.sh
+++ experiments/plot_word_count.sh
@@ -6,6 +6,8 @@ mkdir -p plots
 
 modes=("sudden" "one-by-one" "fluid")
 
+common="set terminal pdf font \", 8\""
+
 # === one-two ===
 experiment=results/${COMMIT}_word_count-constant-one-two
 (
@@ -20,7 +22,7 @@ batch=1000000
 
 for mode in ${modes[@]}; do
   plot="set logscale y; set xlabel \"sec (wall clock)\"; set ylabel \"latency (µsec)\"; set key off; plot \"$experiment/latency-word_count_n2_w1_rounds10_batch${batch}_keys${keys}_constant_${mode}\" using (\$1/1000000000):(\$2/1000) with lines lt rgb \"black\","
-  gnuplot -p -e "set terminal pdf size 2.3,1.1; $plot" > plots/word_count_scaling_n02_w01_constant_${mode}_batch${batch}_keys${keys}_$COMMIT.pdf
+  gnuplot -p -e "$common; set terminal pdf size 2.3,1.1; $plot" > plots/word_count_scaling_n02_w01_constant_${mode}_batch${batch}_keys${keys}_$COMMIT.pdf
 done
 
 multiplot="set logscale y; set ylabel \"latency (µsec)\"; set key off; set yrange [10:10000000]; set multiplot; "
@@ -42,7 +44,7 @@ done
 
 multiplot="$multiplot; set origin 0,0; set size 1,1; set ylabel; unset format y;  unset border; set tic scale 0; set xlabel \"sec (wall clock)\"; set format x \"\"; plot (1/0); "
 
-gnuplot -p -e "set terminal pdf size 3.5,1.1; $multiplot" > plots/word_count_scaling_n02_w01_constant_multiplot_batch${batch}_keys${keys}_$COMMIT.pdf
+gnuplot -p -e "$common; set terminal pdf size 3.5,1.1; $multiplot" > plots/word_count_scaling_n02_w01_constant_multiplot_batch${batch}_keys${keys}_$COMMIT.pdf
 
 # === half-all constant ===
 experiment=results/${COMMIT}_word_count-constant-half-all
@@ -58,7 +60,7 @@ batch=1000000
 
 for mode in ${modes[@]}; do
   plot="set logscale y; set xlabel \"sec (wall clock)\"; set ylabel \"latency (µsec)\"; set key off; plot \"$experiment/latency-word_count_n2_w4_rounds10_batch${batch}_keys${keys}_constant_${mode}\" using (\$1/1000000000):(\$2/1000) with lines lt rgb \"black\","
-  gnuplot -p -e "set terminal pdf size 2.3,1.1; $plot" > plots/word_count_scaling_n02_w04_constant_${mode}_batch${batch}_keys${keys}_$COMMIT.pdf
+  gnuplot -p -e "$common; set terminal pdf size 2.3,1.1; $plot" > plots/word_count_scaling_n02_w04_constant_${mode}_batch${batch}_keys${keys}_$COMMIT.pdf
 done
 
 multiplot="set logscale y; set ylabel \"latency (µsec)\"; set key off; set yrange [10:10000000]; set multiplot; "
@@ -80,7 +82,7 @@ done
 
 multiplot="$multiplot; set origin 0,0; set size 1,1; set ylabel; unset format y;  unset border; set tic scale 0; set xlabel \"sec (wall clock)\"; set format x \"\"; plot (1/0); "
 
-gnuplot -p -e "set terminal pdf size 3.5,1.1; $multiplot" > plots/word_count_scaling_n02_w04_constant_multiplot_batch${batch}_keys${keys}_$COMMIT.pdf
+gnuplot -p -e "$common; set terminal pdf size 3.5,1.1; $multiplot" > plots/word_count_scaling_n02_w04_constant_multiplot_batch${batch}_keys${keys}_$COMMIT.pdf
 
 # === half-all square ===
 experiment=results/${COMMIT}_word_count-square-half-all
@@ -97,7 +99,7 @@ rounds=10
 
 for mode in ${modes[@]}; do
   plot="set logscale y; set xlabel \"sec (wall clock)\"; set ylabel \"latency (µsec)\"; set key off; plot \"$experiment/latency-word_count_n2_w4_rounds${rounds}_batch${batch}_keys${keys}_square_${mode}\" using (\$1/1000000000):(\$2/1000) with lines lt rgb \"black\","
-  gnuplot -p -e "set terminal pdf size 2.3,1.1; $plot" > plots/word_count_scaling_n02_w04_square_${mode}_batch${batch}_keys${keys}_$COMMIT.pdf
+  gnuplot -p -e "$common; set terminal pdf size 2.3,1.1; $plot" > plots/word_count_scaling_n02_w04_square_${mode}_batch${batch}_keys${keys}_$COMMIT.pdf
 done
 
 multiplot="set logscale y; set ylabel \"latency (µsec)\"; set key off; set yrange [10:10000000]; set multiplot; "
@@ -119,4 +121,4 @@ done
 
 multiplot="$multiplot; set origin 0,0; set size 1,1; set ylabel; unset format y;  unset border; set tic scale 0; set xlabel \"sec (wall clock)\"; set format x \"\"; plot (1/0); "
 
-gnuplot -p -e "set terminal pdf size 3.5,1.1; $multiplot" > plots/word_count_scaling_n02_w04_square_multiplot_batch${batch}_keys${keys}_$COMMIT.pdf
+gnuplot -p -e "$common; set terminal pdf size 3.5,1.1; $multiplot" > plots/word_count_scaling_n02_w04_square_multiplot_batch${batch}_keys${keys}_$COMMIT.pdf
