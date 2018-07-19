38aac93c3f4696d8da2c17c48ffae54e1acf378c
Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>
2018-07-19 11:43:14 +0200

    nexmark: Make user configurable
    Signed-off-by: Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>

---
+++ experiments/nexmark/run_all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CLUSTERUSER="${CLUSTERUSER:-andreal}"
+clusterpath="/home/${CLUSTERUSER}/Src/dynamic-scaling-mechanism/nexmark"
+serverprefix="${CLUSTERUSER}@fdr"
+group=4
+
+function run { # command index groups
+#    xterm +hold -e
+    echo python3 -c "import bench; bench.$1($2, $3)" --clusterpath "${clusterpath}" --serverprefix "${serverprefix}" &
+}
+
+function run_group { # name
+    for i in $(seq 0 $(($group - 1)))
+    do
+        run "$1" "$i" "$group"
+    done
+    wait
+}
+
+run_group "non_migrating"
+run_group "exploratory_migrating"
+run_group "exploratory_baseline"
+run_group "exploratory_migrating_single_process"
+run_group "exploratory_bin_shift"
